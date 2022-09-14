32d06a9de0c068ee5abdae54c156408263986461
Adam Shlecter <adam2020.inbox@gmail.com>
2022-09-13 21:41:37 -0700

    yfinance generates a list of close value

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import pprint
 
 
 #microsoft
-#msft = yf.Ticker("MSFT")
+#msft = yf.Ticker("MSFT") #prints a bunch of data about the stock we don't need, but keeping this here in case
 
-ticker = 'msft'
+ticker = 'sg'
 period = '1d'
-interval = "1h"
+interval = "15m"
 
 
 response = yf.download(tickers=ticker,period=period,interval=interval)
@@ -23,20 +23,13 @@ close_values = list(close.values())
 
 first = close_values[0] #testing iteration of first value in close
 
-'''
-Managing Multi-Level Columns
-How to deal with multi-level column names downloaded with yfinance?
-https://stackoverflow.com/questions/63107594/how-to-deal-with-multi-level-column-names-downloaded-with-yfinance/63107801#63107801
-'''
-
 
 def main():
 
   #pprint.pprint(msft.info)
 
-  #pprint.pprint(data)
-  print(close_values)
-  #print(type(close_values)) #prints all the closing values dict_values([257.3399963378906, 254.7899932861328, 255.1649932861328, 256.8299865722656, 255.00999450683594, 253.3699951171875, 252.0, 251.99000549316406])
+  print(close_values) #prints list of close values [257.3399963378906, 254.7899932861328, 255.1649932861328, 256.8299865722656, 255.00999450683594, 253.3699951171875, 252.0, 251.99000549316406]
+
   #print(first)
 
 if __name__ == '__main__':
