9893007ef726b86c302eacc3437a94d1d6d9ccee
Adam Shlecter <adam2020.inbox@gmail.com>
2022-09-13 19:53:59 -0700

    yfinance for stock market historical data api calls

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+
+#https://github.com/ranaroussi/yfinance
+import yfinance as yf
+import pprint
+
+
+#microsoft
+msft = yf.Ticker("MSFT")
+
 def main():
-  print('hello world')
+
+  pprint.pprint(msft.info)
 
 if __name__ == '__main__':
   main()
+
+
